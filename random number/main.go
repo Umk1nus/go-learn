2d1c2159d57d5f9eb2d0e8c25ab79b6455007d69
Umk1nus <umk1namw@gmail.com>
2021-11-19 06:14:34 +0300

    completed

--- random number/main.go
+++ random number/main.go
@@ -17,22 +17,29 @@ func main() {
 	random := rand.Intn(100) + 1
 	fmt.Println("Выбираю число от 1 до 100")
 	fmt.Println("Число выбрано")
-
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Напишите ваше число")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	succeed := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("Количество ваших попыток: ", 10-guesses)
+		fmt.Println("Напишите ваше число")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if random == guess {
+			fmt.Println("Числа совпали")
+			succeed = true
+			break
+		} else {
+			fmt.Println("Числа не совпали")
+		}
 	}
-	fmt.Println(random)
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if random == guess {
-		fmt.Println("Числа совпали")
-	} else {
-		fmt.Println("Числа не совпали")
+	if !succeed {
+		fmt.Println("Попытки закончились, число:", random)
 	}
 }
