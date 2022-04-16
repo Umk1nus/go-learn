a6b4af1d9562cf9634522a5b5b648e262d7278e9
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-04-16 23:58:33 +0300

    added for webrequest

--- webrequest/main.go
+++ webrequest/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-const url = "https://github.com/Umk1nus"
+const url = "https://go.dev/"
 
 func main() {
 
 	response, err := http.Get(url)
-
 	checkNilErr(err)
 
 	fmt.Printf("Тип респонса %T", response)
@@ -19,10 +20,21 @@ func main() {
 	defer response.Body.Close()
 
 	databyte, err := ioutil.ReadAll(response.Body)
-
 	checkNilErr(err)
+
 	content := string(databyte)
 	fmt.Println(content)
+
+	file, err := os.Create("./index.html")
+	checkNilErr(err)
+
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+
+	fmt.Println(length)
+
+	defer file.Close()
+
 }
 
 func checkNilErr(err error) {
