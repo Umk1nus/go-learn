52ec6732d58c9dc31b562c36a1e900e65b491675
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-04-26 11:53:26 +0300

    api learn part 1

---
+++ API/main.go
@@ -0,0 +1,23 @@
+package main
+
+type Course struct {
+	CourseId    string  `json:"courseId"`
+	CourseName  string  `json:"courseName"`
+	CoursePrice int     `json:"coursePrice"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+var courses []Course
+
+func (c *Course) isEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+
+}
