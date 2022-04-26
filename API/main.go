b7ac648ab8e3939100c4a92b6e3a3dcf016133fd
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-04-27 01:17:17 +0300

    api learn part 2

--- API/main.go
+++ API/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Course struct {
 	CourseId    string  `json:"courseId"`
 	CourseName  string  `json:"courseName"`
@@ -16,6 +21,15 @@ var courses []Course
 
 func (c *Course) isEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Message"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
 
 func main() {
